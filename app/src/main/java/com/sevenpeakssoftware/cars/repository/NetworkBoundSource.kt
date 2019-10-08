603729b0b33541675edb429dfc0021e05fbe973d
Adnan Khan <adnan.khan1@telenor.com.pk>
2019-10-08 13:26:40 +0500

    Unit tests added for ArticleApiService.

--- app/src/main/java/com/sevenpeakssoftware/cars/repository/NetworkBoundSource.kt
+++ app/src/main/java/com/sevenpeakssoftware/cars/repository/NetworkBoundSource.kt
@@ -3,15 +3,14 @@ package com.sevenpeakssoftware.cars.repository
 import io.reactivex.Flowable
 import io.reactivex.FlowableEmitter
 import io.reactivex.Single
-import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 
 abstract class NetworkBoundSource<LocalType, RemoteType>(emitter: FlowableEmitter<Resource<LocalType>>) {
 
     init {
         val firstDataDisposable = loadFromDb()
-            .map { it -> Resource.loading<LocalType>(it) }
-            .subscribe { it -> emitter.onNext(it) }
+            .map { Resource.loading<LocalType>(it) }
+            .subscribe { emitter.onNext(it) }
 
         createCall()
             .subscribeOn(Schedulers.io())
@@ -19,22 +18,22 @@ abstract class NetworkBoundSource<LocalType, RemoteType>(emitter: FlowableEmitte
                 { response ->
                     firstDataDisposable.dispose()
                     saveCallResult(response)
-                    loadFromDb().map { it -> Resource.success<LocalType>(it) }
-                        .subscribe(Consumer<Resource<LocalType>> {
+                    loadFromDb().map { Resource.success<LocalType>(it) }
+                        .subscribe {
                             emitter.onNext(it)
-                        })
+                        }
 
-                },
-                {
-                    loadFromDb().map { data ->
-                        Resource.error<LocalType>(it.toString(), data)
+                }
+            ) {
+                loadFromDb().map { data ->
+                    Resource.error<LocalType>(it.toString(), data)
 
+                }
+                    .subscribe {
+                        emitter.onNext(it)
                     }
-                        .subscribe(Consumer<Resource<LocalType>> {
-                            emitter.onNext(it)
-                        })
 
-                })
+            }
     }
 
     abstract fun createCall(): Single<RemoteType>
---
+++ app/src/test/java/com/sevenpeakssoftware/cars/api/ArticleApiServiceTest.kt
@@ -0,0 +1,74 @@
+package com.sevenpeakssoftware.cars.api
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth
+import com.sevenpeakssoftware.cars.utils.mock
+import okhttp3.HttpUrl
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.Okio
+import org.bouncycastle.crypto.tls.ConnectionEnd.client
+import org.hamcrest.core.Is.`is`
+import org.junit.After
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ArticleApiServiceTest {
+
+    @Rule
+    @JvmField
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var service: ArticleApiService
+    private lateinit var mockWebServer: MockWebServer
+
+    @Before
+    fun createService() {
+        mockWebServer = MockWebServer()
+        service = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/"))
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(ArticleApiService::class.java)
+    }
+
+    @After
+    fun stopService() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun getArticles() {
+        enqueueResponse("articles.json")
+        service.getArticles().subscribe({
+            assertThat(it.status, `is`("success"))
+            assertThat(it.content.size, `is`(9))
+
+        }, {
+
+        })
+
+        val request = mockWebServer.takeRequest()
+        assertThat(request.path, `is`("/article/get_articles_list"))
+    }
+
+    private fun enqueueResponse(fileName: String, headers: Map<String, String> = emptyMap()) {
+        val inputStream = javaClass.classLoader
+            ?.getResourceAsStream("api-response/$fileName")
+        val source = Okio.buffer(Okio.source(inputStream))
+        val mockResponse = MockResponse()
+        for ((key, value) in headers) {
+            mockResponse.addHeader(key, value)
+        }
+        mockWebServer.enqueue(
+            mockResponse
+                .setBody(source.readString(Charsets.UTF_8))
+        )
+    }
+}
